f12d4f0a51f4e3ef43b4d16425645b0c13cb7d8c
Sanderhelleso <sanderhelleso@hotmail.com>
2018-07-04 13:04:26 +0200

    customized grid

--- client/src/components/GenreCard.js
+++ client/src/components/GenreCard.js
@@ -2,17 +2,19 @@ import React, { Component } from 'react';
 
 class GenreCard extends Component {
     renderContent() {
-        const categories = ["house", "trance", "edm", "nightcore", "pop", "hip-hop", "rap", "rock", "metall", "country"];
+        const categories = ["house", "trance", "edm", "nightcore", "pop", "hip-hop", "rap", "rock", "metall", "country", "R&B", "indie"];
         return categories.map((category) =>
-            <div key={category} className="container">
+            <div key={category} className="col s4">
+                <div className="z-depth-2 card">
                 <h5>{category}</h5>
+                </div>
             </div>
         );
     }
 
     render() {
         return (
-            <div>
+            <div className="container row">
             {this.renderContent()}
             </div>
         )
