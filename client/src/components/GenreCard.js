83976a3063d5cb25385bc49974b573400e69ce55
Sanderhelleso <sanderhelleso@hotmail.com>
2018-07-04 12:54:52 +0200

    added genreCards to be rendered

--- client/src/components/GenreCard.js
+++ client/src/components/GenreCard.js
@@ -3,13 +3,17 @@ import React, { Component } from 'react';
 class GenreCard extends Component {
     renderContent() {
         const categories = ["house", "trance", "edm", "nightcore", "pop", "hip-hop", "rap", "rock", "metall", "country"];
-        console.log(categories);
+        return categories.map((category) =>
+            <div key={category} className="container">
+                <h5>{category}</h5>
+            </div>
+        );
     }
 
     render() {
         return (
             <div>
-            qweq
+            {this.renderContent()}
             </div>
         )
     }
