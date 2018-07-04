dedea9b8f7d4e3217e85651e7abb46193593024f
Sanderhelleso <sanderhelleso@hotmail.com>
2018-07-04 14:48:01 +0200

    wrapped cards in a link to radio category

--- client/src/components/GenreCard.js
+++ client/src/components/GenreCard.js
@@ -4,16 +4,18 @@ class GenreCard extends Component {
     renderContent() {
         const categories = ["house", "trance", "edm", "nightcore", "pop", "hip-hop", "rap", "rock", "metall", "country", "R&B", "indie"];
         return categories.map((category) =>
-            <div key={category} className="col s4">
-                <div className="z-depth-2 card">
-                    <div className="card-image">
-                        <div className="genreOverlay">
-                            <img className="genreCover" src={`img/${category}.jpg`}/>
+            <a href={`/radio/${category}`}>
+                <div key={category} className="col s4">
+                    <div className="z-depth-2 card">
+                        <div className="card-image">
+                            <div className="genreOverlay">
+                                <img className="genreCover" src={`img/${category}.jpg`}/>
+                            </div>
+                            <span className="card-title genreTitle">{category.toUpperCase()}</span>
                         </div>
-                        <span className="card-title genreTitle">{category.toUpperCase()}</span>
                     </div>
                 </div>
-            </div>
+            </a>
         );
     }
 
