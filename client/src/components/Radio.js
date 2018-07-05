46efaf8023da24c3f5721ae61d4788248333d7f8
Sanderhelleso <sanderhelleso@hotmail.com>
2018-07-05 21:24:46 +0200

    some typo fixs

--- client/src/components/Radio.js
+++ client/src/components/Radio.js
@@ -3,12 +3,10 @@ import Player from '../components/Player';
 
 class Radio extends Component {
     componentWillMount() {
-        //document.body.classList.add("radioBG");
+        document.body.classList.add("radioBG");
         setInterval(() => {
-            //document.body.classList.remove("radioBG");
             const rgb = Math.floor(Math.random() * 255) + 50;
             const opacity = (Math.random() * 0.55) + 0.40;
-            console.log(opacity);
             document.body.style.backgroundImage = `linear-gradient(rgba(${rgb}, 0, 255, 0.8), rgba(0, 255, ${rgb}, ${opacity})), url('/img/background/radio${Math.floor(Math.random() * 10) + 1}.jpg')`;
         }, 10000);
     } 
