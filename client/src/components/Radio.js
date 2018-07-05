5717afbcf82f4fb1e77000498fc2a65ecbde7ed4
Sanderhelleso <sanderhelleso@hotmail.com>
2018-07-05 20:11:25 +0200

    modified download algo

--- client/src/components/Radio.js
+++ client/src/components/Radio.js
@@ -3,7 +3,11 @@ import Player from '../components/Player';
 
 class Radio extends Component {
     componentWillMount() {
-        document.body.classList.add("radioBG");
+        //document.body.classList.add("radioBG");
+        setInterval(() => {
+            //document.body.classList.remove("radioBG");
+            document.body.style.backgroundImage = `linear-gradient(rgba(118, 0, 255, 0.8), rgba(0, 255, 242, 0.95)), url('/img/background/radio${Math.floor(Math.random() * 10) + 1}.jpg')`;
+        }, 10000);
     } 
 
     renderContent() {
--- downloadBg/downloadBg.js
+++ downloadBg/downloadBg.js
@@ -2,15 +2,24 @@ const https = require('https');
 const fs = require('fs');
 const request = require('request');
 
+
+// download img with headers
 function downloadBg(uri, filename, callback) {
     request.head(uri, (err, res, body) => {
-      console.log('content-type:', res.headers['content-type']);
-      console.log('content-length:', res.headers['content-length']);
-  
-      request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
+        if (err) {
+            throw err;
+        }
+        request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
     });
   };
   
-  downloadBg('https://source.unsplash.com/random/1920x1080', './client/public/img/background/player.jpg', () => {
-    console.log('done');
-  });
+  // download new radio wallpaper every min
+  setInterval(() => {
+    for (let i = 1; i < 11; i++) {
+        setTimeout(() => {
+            downloadBg('https://source.unsplash.com/random/1920x1080', `./client/public/img/background/radio${i}.jpg`, () => {
+                console.log('done');
+            });
+        }, i * 6000);
+    };
+  }, 60000);
