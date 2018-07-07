5cc09eefd51a94fc82e040ad6ed80e37630d894f
Sanderhelleso <sanderhelleso@hotmail.com>
2018-07-07 09:00:07 +0200

    finished up sidenav

--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -2,78 +2,39 @@ import React, { Component } from 'react';
 import categories from "../vars/categories";
 
 class Sidebar extends Component {
-    componentWillMount() {
+    // init sidenav
+    componentDidMount() {
         document.addEventListener('DOMContentLoaded', () => {
             const elems = document.querySelectorAll('.sidenav');
             const instances = M.Sidenav.init(elems);
         });
+    }
 
-        console.log(categories);
+    // render sidenav cards
+    renderContent() {
+        return categories.map((category) =>
+            <a key={category} href={`/radio/${category}`}>
+                <li className="user-view">
+                    <div className="background">
+                        <div className="sideCard card">
+                            <div className="card-image">
+                                <div className="genreOverlay">
+                                    <img className="genreCover" src={`/img/${category}.jpg`} alt={`${category}`} />
+                                </div>
+                                <span className="card-title genreTitle">{category.toUpperCase()}</span>
+                            </div>
+                        </div>
+                    </div>
+                </li>
+            </a>
+        );
     }
 
     render() {
         return (
             <ul id="slide-out" className="sidenav">
-                <li className="user-view">
-                    <div className="background">
-                        <img src="/img/landing.jpg" className="responsive-img"></img>
-                    </div>
-                </li>
-                <li className="user-view">
-                    <div className="background">
-                        <img src="/img/landing.jpg" className="responsive-img"></img>
-                    </div>
-                </li>
-                <li className="user-view">
-                    <div className="background">
-                        <img src="/img/landing.jpg" className="responsive-img"></img>
-                    </div>
-                </li>
-                <li className="user-view">
-                    <div className="background">
-                        <img src="/img/landing.jpg" className="responsive-img"></img>
-                    </div>
-                </li>
-                <li className="user-view">
-                    <div className="background">
-                        <img src="/img/landing.jpg" className="responsive-img"></img>
-                    </div>
-                </li>
-                <li className="user-view">
-                    <div className="background">
-                        <img src="/img/landing.jpg" className="responsive-img"></img>
-                    </div>
-                </li>
-                <li className="user-view">
-                    <div className="background">
-                        <img src="/img/landing.jpg" className="responsive-img"></img>
-                    </div>
-                </li>
-                <li className="user-view">
-                    <div className="background">
-                        <img src="/img/landing.jpg" className="responsive-img"></img>
-                    </div>
-                </li>
-                <li className="user-view">
-                    <div className="background">
-                        <img src="/img/landing.jpg" className="responsive-img"></img>
-                    </div>
-                </li>
-                <li className="user-view">
-                    <div className="background">
-                        <img src="/img/landing.jpg" className="responsive-img"></img>
-                    </div>
-                </li>
-                <li className="user-view">
-                    <div className="background">
-                        <img src="/img/landing.jpg" className="responsive-img"></img>
-                    </div>
-                </li>
-                <li className="user-view">
-                    <div className="background">
-                        <img src="/img/landing.jpg" className="responsive-img"></img>
-                    </div>
-                </li>
+                {this.renderContent()};
+                <li className="sideHomeLink"><a href="/" className="white-text center-align">Back to homepage</a></li>
             </ul>
         )
     }
