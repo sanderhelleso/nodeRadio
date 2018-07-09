6c85b01c4ad9da4d622f06ffc925334f6154d72e
Sanderhelleso <sanderhelleso@hotmail.com>
2018-07-09 19:43:09 +0200

    can now adjust volume of radio

--- client/src/components/Volume.js
+++ client/src/components/Volume.js
@@ -1,23 +1,29 @@
 import React, { Component } from 'react'
 
 class Volume extends Component {
-  render() {
-    return (
-        <div id="volumeCont" className="animated fadeIn">
-            <div className="row">
-                <div className="col s1 volIco">
-                    <i className="material-icons volumeDown">volume_down</i>
-                </div>
-                <div id="volumeRow" className="col s10">
-                    <input id="volume" type="range" min="0" max="100" />
-                </div>
-                <div className="col s1 volIco">
-                    <i className="material-icons volumeUp">volume_up</i>
+    render() {
+        return (
+            <div id="volumeCont" className="animated fadeIn">
+                <div className="row">
+                    <div className="col s1 volIco">
+                        <i className="material-icons volumeDown">volume_down</i>
+                    </div>
+                    <div id="volumeRow" className="col s10">
+                        <input id="volume" type="range" min="0" max="100" onChange={volume} />
+                    </div>
+                    <div className="col s1 volIco">
+                        <i className="material-icons volumeUp">volume_up</i>
+                    </div>
                 </div>
             </div>
-        </div>
-    )
-  }
+        )
+    }
+}
+
+function volume() {
+    const volume = document.querySelector("#volume");
+    const player = document.querySelector("#player").childNodes[0];
+    player.volume = volume.value / 100;
 }
 
 export default Volume;
